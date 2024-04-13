6e9396cc013c5570364d9b5de17e56ea4e3afe94
fahmed10 <fahmed10@ggc.edu>
2024-04-13 14:25:47 -0400

    Add getWordCount function

--- Main.java
+++ Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static int getWordCount(String str) {
+        return str.split(" ").length;
+    }
 }
